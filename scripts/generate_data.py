7880f90245ce92e37e8fd778dfb12d1c8042cd08
Yari D'Onofrio <6898510+EngineerDogIta@users.noreply.github.com>
2022-01-10 17:06:41 +0100

    25 refactoring data generation script (#51)
    * Refractoring of AwesomeEvent into awesome_event.py
    
    Refractoring of data moved, fixed the Regex for parsing the lines, created methods for the AwesomeEvent class
    
    * Fixed Location with description
    
    * Fixed generation of events for Title only instead of links
    
    Co-authored-by: EngineerDogIta <bellayari@gmx.com>

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,4 +1,3 @@
-import shutil
 import re
 import json
 import codecs
@@ -8,51 +7,26 @@ from datetime import date, timedelta
 from icalendar import Calendar, Event
 from pathlib import Path
 
-
-class AwesomeEvent:
-    def __init__(
-        self,
-        title,
-        url,
-        description,
-        location,
-        startDate,
-        endDate=None
-    ):
-        self.title = title
-        self.url = url
-        self.description = description
-        self.location = location
-        self.startDate = str(startDate)
-        self.endDate = str(endDate or startDate)
-
-
-def obj_dict(obj):
-    return obj.__dict__
+from scripts.awesome_event import AwesomeEvent
 
 
 def parse_data(path):
     with codecs.open(path, 'r', encoding='utf-8-sig') as raw:
-        rawLines = raw.readlines()
+        raw_lines = raw.readlines()
 
-    heading = re.compile(
-        r'# Awesome Events in Italy \(([0-9]{4}) Edition\)').split(rawLines[0])
+    heading = re.compile(r'# Awesome Events in Italy \(([0-9]{4}) Edition\)').split(raw_lines[0])
     tokens = list(filter(lambda i: i != '', heading))
-    year = int(tokens[0])
+    current_file_year = int(tokens[0])
 
-    lista = list(map(lambda x: x.replace(os.linesep, ''), rawLines))
+    lista = list(map(lambda x: x.replace(os.linesep, ''), raw_lines))
 
     month = 0
-    events = []
-    debugIndex = lista.index('## January') - 1
+    events_recovered = []
+    debug_index = lista.index('## January') - 1
 
-    for line in lista[lista.index('## January'):
-                      len(lista) - 1 - lista[::-1].index('---')]:
+    for line in lista[lista.index('## January'): len(lista) - 1 - lista[::-1].index('---')]:
         line = line + ''
-        debugIndex += 1
-
-        startDate = None
-        endDate = None
+        debug_index += 1
 
         if line.strip() == '':
             continue
@@ -61,85 +35,51 @@ def parse_data(path):
             month = strptime(line[3:], '%B').tm_mon
             continue
 
-        chunks = re.compile(
-            r'\-\ ([^]]*)\ \-\ \[([^]]*)\]\(([^\s^\)]*)[\s\)]\ \-\ ([^]]*)\ \-\ ([^]]*)\.').split(line)
-
-        params = list(filter(lambda i: i != '', chunks))
-
-        if len(params) != 5:
-            raise ValueError(f'line {debugIndex}: {line}')
-
-        if params[0].find('-') > 0:
-            days = params[0].split('-')
-            startDate = date(year, month, int(days[0]))
-
-            if days[1].isdigit():
-                endDate = date(year, month, int(days[1]))
-            else:
-                endDay = days[1].split(' ')
-                endMonth = month = strptime(endDay[0], '%B').tm_mon
-                endYear = year
-
-                if endMonth < month:
-                    endYear += 1
-
-                endDate = date(endYear, endMonth, int(endDay[1]))
-
-        else:
-            startDate = date(year, month, int(params[0]))
-
-        events.append(
-            AwesomeEvent(
-                title=params[1],
-                url=params[2],
-                location=params[3],
-                description=params[4],
-                startDate=startDate,
-                endDate=endDate
-            )
-        )
-    return (events, year)
+        try:
+            awesome_event = AwesomeEvent.build_by_parsing(current_file_year, month, line)
+            events_recovered.append(awesome_event.to_dict())
+        except ValueError:
+            print(f'Error values in line {debug_index}\n {line}')
+            continue
+    return events_recovered, current_file_year
 
 
 def write_json(events, year, dataDir):
     # write json
     with codecs.open(f'{dataDir}{year}.json', 'w', encoding='utf-8-sig') as jsonFile:
-        json.dump(events, jsonFile, indent=4, default=obj_dict,
-                  ensure_ascii=False)
+        json.dump(events, jsonFile, indent=4, ensure_ascii=False)
 
 
-def write_ical(events, year, dataDir):
+def write_ical(events, year, data_dir):
     # write iCal
     cal = Calendar()
 
-    for awesomeEvent in events:
+    for awesome_event in events:
         event = Event()
 
-        event.add('summary', awesomeEvent.title)
-        event.add('dtstart', date.fromisoformat(awesomeEvent.startDate))
-        event.add('dtend', date.fromisoformat(
-            awesomeEvent.endDate) + timedelta(days=1))
-        event.add('description',
-                  f'{awesomeEvent.description} - {awesomeEvent.url}')
-        event.add('location', awesomeEvent.location)
+        event.add('summary', awesome_event['title'])
+        event.add('dtstart', date.fromisoformat(awesome_event['startDate']))
+        event.add('dtend', date.fromisoformat(awesome_event['endDate']) + timedelta(days=1))
+        event.add('description', f"{awesome_event['description']} - {awesome_event['url']}")
+        event.add('location', awesome_event['location'])
         cal.add_component(event)
 
-    with open(f'{dataDir}{year}.ics', 'wb') as ics:
+    with open(f'{data_dir}{year}.ics', 'wb') as ics:
         ics.write(cal.to_ical())
 
 
 files = ['../README.md', '../2022.md', '../archive/2020.md', '../archive/2019.md']
-dataDir = '../data/'
+data_dir = '../data/'
 
-if not os.path.exists(dataDir):
-    os.mkdir(dataDir)
+if not os.path.exists(data_dir):
+    os.mkdir(data_dir)
 
 for file in files:
     try:
-        (events, year) = parse_data(file)
-        for p in Path(dataDir).glob(f'{year}.*'):
+        events, year = parse_data(file)
+        for p in Path(data_dir).glob(f'{year}.*'):
             p.unlink()
-        write_json(events, year, dataDir)
-        write_ical(events, year, dataDir)
+        write_json(events, year, data_dir)
+        write_ical(events, year, data_dir)
     except ValueError as e:
         raise Exception(f'Parse failed {e.args} - ({file})')
